57da29e69299a1f5ffea5dc728c5ce95d3aab6b5
Hallvard Ystad <hallvard.ystad@babelserver.org>
2026-01-19 13:14:22 +0100

    Add configuration to publish a plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
-    `maven-publish`
-    id("org.owasp.dependencycheck") version "12.1.0"
+    id("com.gradle.plugin-publish") version "2.0.0"
 }
 
 group = "org.babelserver.gradle"
@@ -19,12 +18,20 @@ dependencies {
 }
 
 gradlePlugin {
+    website = "https://github.com/hallyhaa/gradle-test-logger"
+    vcsUrl = "https://github.com/hallyhaa/gradle-test-logger.git"
+
     plugins {
         create("testLogger") {
             id = "org.babelserver.gradle.test-logger"
             implementationClass = "org.babelserver.gradle.testlogger.TestLoggerPlugin"
             displayName = "Test Logger Plugin"
             description = "Provides pretty test output with status indicators for each test"
+            tags = listOf(
+                "testing", "test", "logging", "output", "reporting",
+                "kotlin", "kotlin-multiplatform", "kotlin-js", "kotlin-native",
+                "junit", "test-results"
+            )
         }
     }
 }
